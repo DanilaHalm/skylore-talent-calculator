52ff66c1a32649d43768c9f2bcb15932d23c010b
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-11-26 13:21:06 +0300

    Update shards.jsx

--- src/app/components/shards.jsx
+++ src/app/components/shards.jsx
@@ -1,17 +1,17 @@
 "use client"
 import { useState } from "react";
 
-const Shards = () => {
-  const [checked, setChecked] = useState(false)
-
+const Shards = (selectedShard) => {
+  const [checked, setChecked] = useState(selectedShard)
+  
   const changeState = () => {
-    setChecked(!checked)
+    setChecked(checked === 1? 2 : 1)
   }
   
   return (
     <>
-      <input type="radio" checked={checked} onclick={()=> changeState}></input>
-      <input type="radio" checked={checked} onclick={()=> changeState}></input>
+      <input type="radio" checked={checked === 1} onclick={()=> changeState}></input>
+      <input type="radio" checked={checked === 2} onclick={()=> changeState}></input>
     </>
   )
 }
