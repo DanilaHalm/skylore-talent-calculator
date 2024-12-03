7a8e1824471ccb3cdee77de99325e3650b2cc0af
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-12-03 13:45:38 +0300

    Update classlink.jsx

--- src/app/components/classlink.jsx
+++ src/app/components/classlink.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const ClassLink = async ({ class_link }) => {
   //const img = await import(`/public/${class_link}.png`)
   return (
-    <div className={`w-14 h-14 bg-[url('/public/${class_link}.png')] border-2 bg-cover rounded-full`}>
+    <div className={`w-14 h-14 bg-[url('../public/${class_link}.png')] border-2 bg-cover rounded-full`}>
       <Link className="w-14 h-14 block rounded-full" href={`/${class_link.toLowerCase()}`}>
         {class_link}
       </Link>
