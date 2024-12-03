79bb3c6ef1d7650e8efbd88f212c341b511dd56e
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-12-03 14:40:13 +0300

    Update classlink.jsx

--- src/app/components/classlink.jsx
+++ src/app/components/classlink.jsx
@@ -8,7 +8,7 @@ const ClassLink = async ({ class_link }) => {
   const isActive = pathname === `/${class_link.toLowerCase()}` ? "active_class" : ""
  
   return (
-    <div className={`w-14 h-14 ${class_link}-bg isActive border-2 bg-cover bg-no-repeat bg-center rounded-full`}>
+    <div className={`w-14 h-14 ${class_link}-bg isActive grayscale border-2 bg-cover bg-no-repeat bg-center rounded-full`}>
       <Link className="w-14 h-14 block rounded-full" href={`/${class_link.toLowerCase()}`}>
         
         
