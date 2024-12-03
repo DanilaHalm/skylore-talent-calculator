cea7fce655f6e5541c7a81d0e79c8210680307a2
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-12-03 13:24:06 +0300

    Update classlink.jsx

--- src/app/components/classlink.jsx
+++ src/app/components/classlink.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const ClassLink = ({ class_link }) => {
   return (
-    <div className={`w-14 h-14 bg-[url('../../public/${class_link}.png')] bg-gray-200 bg-cover rounded-full`}>
+    <div className={`w-14 h-14 bg-gray-200 bg-cover rounded-full`}>
       <Link className="w-14 h-14 block rounded-full" href={`/${class_link.toLowerCase()}`}>
         {class_link}
       </Link>
