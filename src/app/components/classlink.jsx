c394082c2d496e0bed7e969efb3d1b48d14b30e6
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-12-03 14:20:15 +0300

    Update classlink.jsx

--- src/app/components/classlink.jsx
+++ src/app/components/classlink.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const ClassLink = async ({ class_link }) => {
   const params = useParams()
   return (
-    <div className={`w-14 h-14 ${class_link}-bg border-2 bg-cover bg-no-repeat bg-center rounded-full`}>
+    <div className={`w-14 h-14 ${class_link}-bgg border-2 bg-cover bg-no-repeat bg-center rounded-full`}>
       <Link className="w-14 h-14 block rounded-full" href={`/${class_link.toLowerCase()}`}>
         <code>{params}</code>
         
