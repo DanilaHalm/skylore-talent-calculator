e2a0c8bdd15921b4a5bd72086e97c1c4bc757a64
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-12-04 10:48:02 +0300

    Update lvlbar.jsx

--- src/app/components/lvlbar.jsx
+++ src/app/components/lvlbar.jsx
@@ -9,12 +9,14 @@ const LevelBar = () => {
   const [curLevel,setCurLevel] = useState(searchParams.get("lvl"))
   
   const levels = ["20","23","26","29","32","35"]
+  const maxShards = {"20":"24","23":"26","26":"29","29":"31","32":"34","35":"36"},
   
   const createQueryString = useCallback(
     (lvl) => {
       const params = new URLSearchParams(searchParams);
       params.set("lvl", lvl);
-
+      params.set("max", maxShards[lvl]);
+      
       return params.toString();
     },
     [searchParams]
