72e9f58184974be31f8507a5c643fa2be01894cf
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-12-04 09:51:50 +0300

    Create lvlbar.jsx

---
+++ src/app/components/lvlbar.jsx
@@ -0,0 +1,47 @@
+"use client";
+import { useCallback, useState } from "react";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
+const LevelBar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [curLevel,setCurLevel] = useState(20)
+  
+  const levels = [20,23,26,29,32,35]
+  
+  const createQueryString = useCallback(
+    (values) => {
+      const params = new URLSearchParams(searchParams);
+      params.set("lvl", values.lvl);
+      params.set("shards", values.shards);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  const createString = (event) => {
+    const value = JSON.parse(event.target.value);
+    return pathname + "?" + createQueryString(value);
+  };
+
+  const handleChange = (event) => {
+    //router.push(createString(event));
+    setCurLevel(event.target.value)
+  };
+
+  return (
+    <select value={curLevel} onChange={(ev) => handleChange(ev)}>
+      {levels.map((level) => {
+        return (
+          <option key={`option${level}`} value={level}>
+            {level}
+          </option>
+        );
+      })}
+    </select>
+  );
+};
+
+export default LevelBar;
