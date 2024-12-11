c281b0be06ea12e48e1c688f67993855139f1c03
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-12-11 23:17:01 +0300

    Create hunterSkills.js

---
+++ src/app/ lib/hunterSkills.js
@@ -0,0 +1,68 @@
+const hunterSkills = [
+  {
+    name: "name",
+    description:"description",
+    requiredLvl:"reqLvl",
+    icon:"icon.jpg",
+    shards: [],
+  },
+  {
+    name: "name",
+    description:"description",
+    requiredLvl:"reqLvl",
+    icon:"icon.jpg",
+    shards: [],
+  },
+  {
+    name: "name",
+    description:"description",
+    requiredLvl:"reqLvl",
+    icon:"icon.jpg",
+    shards: [],
+  },
+  {
+    name: "name",
+    description:"description",
+    requiredLvl:"reqLvl",
+    icon:"icon.jpg",
+    shards: [],
+  },
+  {
+    name: "name",
+    description:"description",
+    requiredLvl:"reqLvl",
+    icon:"icon.jpg",
+    shards: [],
+  },
+  {
+    name: "name",
+    description:"description",
+    requiredLvl:"reqLvl",
+    icon:"icon.jpg",
+    shards: [],
+  },
+  {
+    name: "name",
+    description:"description",
+    requiredLvl:"reqLvl",
+    icon:"icon.jpg",
+    shards: [],
+  },
+  {
+    name: "name",
+    description:"description",
+    requiredLvl:"reqLvl",
+    icon:"icon.jpg",
+    shards: [],
+  },
+  {
+    name: "name",
+    description:"description",
+    requiredLvl:"reqLvl",
+    icon:"icon.jpg",
+    shards: [],
+  },
+  
+]
+
+export default hunterSkills
