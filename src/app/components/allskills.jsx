7669bf5e9a2bf21288c7ea81bf591b4dc6cb3e34
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-03-10 23:29:36 +0300

    Update allskills.jsx

--- src/app/components/allskills.jsx
+++ src/app/components/allskills.jsx
@@ -2,9 +2,9 @@ const AllSkills = ({skills,setSkills}) => {
   
   
   return (
-    <div className="flex justify-around flex-wrap">
+    <div className="flex justify-between flex-wrap">
       {skills.map((skill) => {
-      return skill.index!==0 && <div key={skill.name} className="w-12 h-12 bg-blue-200">{skill.index}</div>
+      return skill.index!==0 && <div key={skill.name} className="w-14 h-14 bg-blue-200">{skill.index}</div>
       })}
     </div>
   )
