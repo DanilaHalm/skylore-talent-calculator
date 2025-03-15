583046e9b51e3a2b1c6a0c7b0a16bec6dec8169d
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-03-15 09:49:03 +0300

    input

--- src/app/components/allskills.jsx
+++ src/app/components/allskills.jsx
@@ -4,7 +4,11 @@ const AllSkills = ({skills,setSkills}) => {
   return (
     <div className="grid grid-cols-4 gap-4 justify-center">
       {skills.map((skill) => {
-      return skill.index!==0 && <div key={skill.name} className="w-14 h-14 bg-blue-200">{skill.index}</div>
+      return (skill.index!==0 && 
+      <div key={skill.name} className="w-14 h-14 bg-blue-200">
+        <Input type="checkbox" className="invisible w-14 h-14 bg-green-200"/>
+      </div>
+    )
       })}
     </div>
   )
