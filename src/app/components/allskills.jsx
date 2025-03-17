a854a1f8708344ebfb944b8ee0d3db1749fae337
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-03-17 15:53:43 +0300

    Update allskills.jsx

--- src/app/components/allskills.jsx
+++ src/app/components/allskills.jsx
@@ -6,6 +6,10 @@ const updated = [...skills]
 updated[index].checked = !updated[index].checked
 setSkills(updated)
 }
+  const checkActive = () => {
+  const filtered = skills.filter(skill=> skill.checked === true)
+  return filtered.length > 4 ? false : true
+}
   
   return (
     <div className="grid grid-cols-4 gap-4 justify-center">
@@ -13,8 +17,8 @@ setSkills(updated)
       {skills.map((skill) => {
       return (skill.index!==0 && 
       <div key={skill.name} className={`w-14 h-14 ${skill.checked? "bg-blue-800" : "bg-blue-200"}`}>
-      {`${skill.index}`}
-        <input type="checkbox" onClick={()=> checkSkill(skill.index)} checked={skill.checked} className="opacity-0 w-14 h-14 bg-green-200"/>
+      
+        <input type="checkbox" onClick={()=> checkSkill(skill.index)} disabled={checkActive()} checked={skill.checked} className="opacity-0 w-14 h-14 bg-green-200"/>
       </div>
     )
       })}
