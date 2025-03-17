205ddb8c51e516dd947505e74b67173e195172a1
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-03-17 13:32:01 +0300

    Update allskills.jsx

--- src/app/components/allskills.jsx
+++ src/app/components/allskills.jsx
@@ -8,9 +8,9 @@ setSkills(...skills)
     <div className="grid grid-cols-4 gap-4 justify-center">
       {skills.map((skill) => {
       return (skill.index!==0 && 
-      <div key={skill.name} className={`w-14 h-14 ${skill.checked? "bg-blue-800" : "bg-blue-200"}`}>
+      <div onClick={()=> checkSkill(skill)}  key={skill.name} className={`w-14 h-14 ${skill.checked? "bg-blue-800" : "bg-blue-200"}`}>
       {`${skill.checked}`}
-        <input onClick={()=> checkSkill(skill)} type="checkbox" checked={skill.checked} className="opacity-0 w-14 h-14 bg-green-200"/>
+        <input type="checkbox" checked={skill.checked} className="opacity-0 w-14 h-14 bg-green-200"/>
       </div>
     )
       })}
