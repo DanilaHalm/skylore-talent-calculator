ca8d8c8a5eaec402ce10ab34406aba5465365e89
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-03-17 23:20:40 +0300

    Update chosedSkills.jsx

--- src/app/components/chosedSkills.jsx
+++ src/app/components/chosedSkills.jsx
@@ -3,7 +3,9 @@ const ChosedSkills = ({skills}) => {
     <div>
   {skills.map(skill => {
   return (
-  <div className="h-[20vh] bg-yellow-200" key={skill.name+"add"}></div>
+  <div className="h-[20vh] bg-yellow-200 border" key={skill.name+"add"}>
+  
+</div>
 )
 })}
 </div>
