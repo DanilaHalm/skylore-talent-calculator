376f9ac4deb5975f56e1a25276710cac4116243e
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-03-18 22:51:39 +0300

    Update chosedSkills.jsx

--- src/app/components/chosedSkills.jsx
+++ src/app/components/chosedSkills.jsx
@@ -6,6 +6,7 @@ const ChosedSkills = ({skills}) => {
   <div className="flex flex-col w-screen h-[20vh] bg-yellow-200 border" key={skill.name+"add"}>
     <div className="flex h-1/2 bg-orange-200">
   <div className="h-full aspect-square bg-blue-200">img</div>
+  <div className="grow bg-purple-300"></div>
 </div>
 </div>
 )
