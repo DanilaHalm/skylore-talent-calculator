6ff0f95ecbdfa873eebc8139409ffc616c0ae4f1
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-03-18 23:00:48 +0300

    Create skillShards.jsx

---
+++ src/app/components/skillShards.jsx
@@ -0,0 +1,15 @@
+import Shards from "./shards";
+
+const SkillShards = () => {
+  return (
+    <div className="grow bg-purple-300">
+      <div className="bg-red-400">
+        <Shards selectedShard={0}/>
+        <Shards selectedShard={0}/>
+        <Shards selectedShard={0}/>
+      </div>
+</div>
+)
+}
+
+export default SkillShards
