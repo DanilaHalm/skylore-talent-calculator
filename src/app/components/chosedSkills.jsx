fa48b3a1c40a592659b2908e84da30b097d5c370
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-03-18 23:12:30 +0300

    Update chosedSkills.jsx

--- src/app/components/chosedSkills.jsx
+++ src/app/components/chosedSkills.jsx
@@ -3,18 +3,18 @@ import SkillShards from "./skillShards";
 const ChosedSkills = ({skills}) => {
   return (
     <div>
-  {skills.map(skill => {
-  return (
-  <div className="flex flex-col w-screen h-[20vh] bg-yellow-200 border" key={skill.name+"add"}>
-    <div className="flex flex-row h-1/2 bg-orange-200">
-  <div className="h-full aspect-square bg-blue-200">img</div>
-  <SkillShards/>
-</div>
-</div>
-)
-})}
-</div>
-)
+      {skills.map(skill => {
+        return (
+          <div className="flex flex-col w-screen h-[20vh] bg-yellow-200 border" key={skill.name+"add"}>
+            <div className="flex flex-row h-1/2 bg-orange-200">
+              <div className="h-full aspect-square bg-blue-200">img</div>
+              <SkillShards/>
+            </div>
+          </div>
+         )
+       })}
+     </div>
+   )
 }
 
 export default ChosedSkills
