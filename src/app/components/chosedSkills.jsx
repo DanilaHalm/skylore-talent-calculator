c57f1646af363e326bfa7b279dc685b6db13975f
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-03-21 23:34:40 +0300

    Update chosedSkills.jsx

--- src/app/components/chosedSkills.jsx
+++ src/app/components/chosedSkills.jsx
@@ -1,16 +1,20 @@
+"use client"
+import {useState} from "react";
 import SkillShards from "./skillShards";
 
 const ChosedSkills = ({skills}) => {
+  const [isDescActive, setIsDescActive] = useState(false)
+
   return (
     <div>
       {skills.map(skill => {
         return (
-          <div className="flex flex-col w-screen h-[20vh] bg-yellow-200 border" key={skill.name+"add"}>
-            <div className="flex flex-row h-1/2 bg-orange-200">
-              <div className="h-full aspect-square bg-blue-200">img</div>
+          <div className="flex flex-col w-screen bg-yellow-200 border" key={skill.name+"add"}>
+            <div className="flex flex-row h-[20vh] bg-orange-200">
+              <div onClick={()=>setIsDescActive (!isDescActive)} className="h-full aspect-square bg-blue-200">img</div>
               <SkillShards/>
             </div>
-            <div>description</div>
+            <div className={`${isDescActive? h-auto : h-0}`}>description</div>
           </div>
          )
        })}
