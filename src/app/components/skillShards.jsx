214d8569a2b4e634f12e8a8ed9414b1847802067
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-04-02 10:05:54 +0300

    Update skillShards.jsx

--- src/app/components/skillShards.jsx
+++ src/app/components/skillShards.jsx
@@ -2,7 +2,7 @@ import Shards from "./shards";
 
 const SkillShards = () => {
   return (
-    <div className="grow bg-purple-300">
+    <div className="h-1/2 grow bg-purple-300">
       <div className="flex flex-row bg-red-400 h-full">
         <Shards selectedShard={0}/>
         <Shards selectedShard={0}/>
