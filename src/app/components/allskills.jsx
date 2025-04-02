bf4320b677d871bbdeb598137396ac2c0da59f04
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-04-02 13:58:04 +0300

    Update allskills.jsx

--- src/app/components/allskills.jsx
+++ src/app/components/allskills.jsx
@@ -30,19 +30,23 @@ const AllSkills = ({skills,setSkills}) => {
       router.push(url)
 }, [searchParams])
 
-  const checkActive = () => {
-  const filtered = skills.filter(skill=> skill.checked === true)
-  return filtered.length > 3 ? true : false
-}
+  //const checkActive = () => {
+  //const filtered = skills.filter(skill=> skill.checked === true)
+  //return filtered.length > 3 ? true : false
+//}
   
   return (
     <div className="grid grid-cols-4 gap-4 justify-around">
 
       {skills.map((skill) => {
       return (skill.index!==0 && 
-      <div key={skill.name} className={`w-14 h-14 ${skill.checked? "bg-blue-800" : "bg-blue-200"}`}>
+      <div key={skill.name} className={`w-14 h-14 bg-blue-200`}>
       
-        <input type="checkbox" onClick={()=> checkSkill(skill.index)} disabled={skill.checked === false? checkActive() : false} checked={skill.checked} className="opacity-0 w-14 h-14 bg-green-200"/>
+        <input type="checkbox" 
+onClick={()=> checkSkill(skill.index)}
+// disabled={skill.checked === false? checkActive() : false} 
+checked={skill.checked} 
+className="opacity-0 w-14 h-14 bg-green-200"/>
       </div>
     )
       })}
