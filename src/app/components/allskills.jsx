c84821107ae39b6a3d9645b9a7620ce7306bf7c6
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-04-03 13:55:23 +0300

    Update allskills.jsx

--- src/app/components/allskills.jsx
+++ src/app/components/allskills.jsx
@@ -12,64 +12,56 @@ const AllSkills = ({skills,setSkills}) => {
 
 
   const setSkill = useCallback((index)=>{
-    
-    
+
     const urlSkills = searchParams.get("skills").split("-")
     const activeSkill = urlSkills.find((skill) => skill[0] === `${index}`)
-    
     
    if(activeSkill){
      const idx = urlSkills.indexOf(activeSkill)
      urlSkills.splice(idx,1)
      
     } else {
-    urlSkills.push(`${index}000`)
+      urlSkills.push(`${index}000`)
     }
     const url = `${pathname}?skills=${urlSkills.join("-")}`
-      router.push(url)
+    router.push(url)
 
 },[searchParams])
   
   
-
-
-
   const checkSkill = (index) => {
-    //const updated = [...skills]
-   // updated[index].checked = !updated[index].checked
-    //setSkills(updated)
-
-    
-
-    
+    const urlSkillsIdx = searchParams.get("skills").split("-").map(skill=> skill[0])
+    const isActive = urlSkillsIdx.includes(index)
+    if(isActive){
+      return false
+    } else {
+      return urlSkillsIdx.length > 4 ? true : false
+    }
 }
 
-  //const checkActive = () => {
-  //const filtered = skills.filter(skill=> skill.checked === true)
-  //return filtered.length > 3 ? true : false
-//}
+  const checkActive = () => {
+    const urlSkillsIdx = searchParams.get("skills").split("-").map(skill=> skill[0])
+    const isActive = urlSkillsIdx.includes(index)
+   return isActive 
+  }
   
   return (
     <div className="grid grid-cols-4 gap-4 justify-around">
 
       {skills.map((skill) => {
       return (skill.index!==0 && 
-      <div key={skill.name} className={`w-14 h-14 bg-blue-200`}>
+      <div key={skill.name} className={`w-14 h-14 ${checkActive(skill.index) ? bg-blue-800 : bg-blue-200} `}>
       
         
           <input type="checkbox" 
-onClick={()=> setSkill(skill.index)} className="opacity-0 w-14 h-14 bg-green-200"/>
+onClick={()=> setSkill(skill.index)}
+disabled={checkSkill(skill.index)} 
+className="opacity-0 w-14 h-14 bg-green-200"/>
       </div>
     )
       })}
     </div>
   )
 }
-
-//<input type="checkbox" 
-//onClick={()=> setSkill(skill.index)}
-//disabled={skill.checked === false? checkActive() : false} 
-//checked={skill.checked} 
-//className="opacity-0 w-14 h-14 bg-green-200"/>
 
 export default AllSkills
