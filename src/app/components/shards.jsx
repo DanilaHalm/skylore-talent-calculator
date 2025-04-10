b179dab085e535571b5bb7a56bd3dfb2deaf204d
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-04-10 15:18:27 +0300

    Update shards.jsx

--- src/app/components/shards.jsx
+++ src/app/components/shards.jsx
@@ -9,10 +9,10 @@ const Shards = ({skillIndex,shardIndex}) => {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
-  const setShard = useCallback((index,e)=>{
+  const setShard = useCallback((e)=>{
     const shardId = e.target.dataset.id
     const urlSkills = searchParams.get("skills").split("-")
-    const activeSkill = urlSkills.find((skill) => skill[0] === `${index}`)
+    const activeSkill = urlSkills.find((skill) => skill[0] === `${skillIndex}`)
     const activeSkillIdx = urlSkills.indexOf(activeSkill)
     activeSkill[shardIndex] = shardId
     urlSkills.splice(activeSkillIdx,1,activeSkill)
@@ -22,9 +22,9 @@ const Shards = ({skillIndex,shardIndex}) => {
 },[searchParams])
   
   
-  const checkShard = (index) => {
+  const checkShard = () => {
     const urlSkills = searchParams.get("skills").split("-")
-    const activeSkill = urlSkills.find((skill) => skill[0] === `${index}`)
+    const activeSkill = urlSkills.find((skill) => skill[0] === `${skillIndex}`)
     if(activeSkill[shardIndex] === 1 && activeShardId === 1) return true
     if(activeSkill[shardIndex] === 2 && activeShardId === 2) return true
     return false
@@ -33,8 +33,8 @@ const Shards = ({skillIndex,shardIndex}) => {
 
   return (
     <div className="flex flex-col justify-around h-[90%] w-7 pl-1 pt-1">
-      <input type="radio" data-id={1} checked={checkShard(skillIndex)} onClick={(e)=>setShard(skillIndex,e)}></input>
-      <input type="radio" data-id={2} checked={checkShard(skillIndex)} onClick={(e)=>setShard(skillIndex,e)}></input>
+      <input type="radio" data-id={1} checked={checkShard()} onClick={(e)=>setShard(e)}></input>
+      <input type="radio" data-id={2} checked={checkShard()} onClick={(e)=>setShard(e)}></input>
     </div>
   )
 }
