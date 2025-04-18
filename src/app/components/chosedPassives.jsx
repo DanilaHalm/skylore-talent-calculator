a22b2cb1e20981bb171c8d82333937dae8f6061f
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-04-18 15:51:36 +0300

    Create chosedPassives.jsx

---
+++ src/app/components/chosedPassives.jsx
@@ -0,0 +1,31 @@
+"use client"
+
+import {useState} from "react";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
+const ChosedPassives = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const [isDescActive, setIsDescActive] = useState(false)
+  const urlSkills = searchParams.get("skills").split("-").map(skill=>skill[0])
+  
+  const passives = [1,2,3]
+
+
+  return (
+    <div className="flex flex-col w-screen">
+      <div className="h-[15vh] bg-green-500 flex flex-row justify-around">
+        { passives.map(passive=> {
+            return <div className="h-[10vh] aspect-square bg-purple-500 rounded" onClick={()=> setIsDescActive(!isDescActive)}></div>
+          })
+        }
+      </div>
+      {
+        isDescActive && <div>{"desc"}</div>
+      }
+    </div>
+   )
+}
+
+export default ChosedPassives
