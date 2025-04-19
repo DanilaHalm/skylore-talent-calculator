7c7e66485c7ae16552c113b1c80d9389035a37b6
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-04-19 18:27:13 +0300

    Update chosedPassives.jsx

--- src/app/components/chosedPassives.jsx
+++ src/app/components/chosedPassives.jsx
@@ -8,21 +8,60 @@ const ChosedPassives = () => {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const [isDescActive, setIsDescActive] = useState(false)
-  const urlSkills = searchParams.get("skills").split("-").map(skill=>skill[0])
+  const urlSkills = searchParams.get("passives").split("")
   
-  const passives = [1,2,3]
+ const passives = [
+  { name: "Pname0",
+    index: 0,
+    desc: "description0"
+  },
+  { name: "Pname1",
+    index: 1,
+    desc: "description1"
+  },
+  { name: "Pname2",
+    index: 2,
+    desc: "description2"
+  },
+  { name: "Pname3",
+    index: 3,
+    desc: "description3"
+  },
+  { name: "Pname4",
+    index: 4,
+    desc: "description4"
+  },
+  { name: "Pname5",
+    index: 5,
+    desc: "description5"
+  },
+  { name: "Pname6",
+    index: 6,
+    desc: "description6"
+  },
+  { name: "Pname7",
+    index: 7,
+    desc: "description7"
+  },
+]
+
+  const chosed = passives.filter(skill=> urlSkills.includes(`${skill.index}`))
 
 
   return (
     <div className="flex flex-col w-screen">
       <div className="h-[12vh] bg-green-500 flex flex-row justify-around items-center">
-        { passives.map(passive=> {
-            return <div className="h-[8vh] aspect-square bg-purple-500 rounded-full" onClick={()=> setIsDescActive(!isDescActive)}></div>
+        { chosed.map(passive=> {
+            return <div key={passive.name} className="h-[8vh] aspect-square bg-purple-500 rounded-full" onClick={()=> setIsDescActive(!isDescActive)}></div>
           })
         }
       </div>
       {
-        isDescActive && <div>{"desc"}</div>
+        isDescActive && <div className="flex flex-col">
+          chosed.map(passive=> {
+            return <div key={passive.desc}>{passive.desc}</div>
+          })
+        </div>
       }
     </div>
    )
