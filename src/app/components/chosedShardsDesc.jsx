1a8d95e0a39425f9ba265c7764aa4ab3fb7547f3
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-13 18:48:05 +0300

    Update chosedShardsDesc.jsx

--- src/app/components/chosedShardsDesc.jsx
+++ src/app/components/chosedShardsDesc.jsx
@@ -5,17 +5,15 @@ const ChosedShardsDesc = (skill) => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
 
-  const urlSkills = searchParams
-    .get("skills")
-    .split("-")
+  const urlSkills = searchParams.get("skills").split("-")
   
-  const selectedSkill = urlSkills.find(skill => skill[0] === `${skill.index}`)
+  const selectedSkill = urlSkills.find(skill => skill[0] === `${skill.index}`);
+
   const shards = [
     skill.shards.tier1[selectedSkill[1]],
     skill.shards.tier2[selectedSkill[2]],
     skill.shards.tier3[selectedSkill[3]]
   ]
-
 
   return (
     <div className="h-full w-1/2  bg-red-600">
