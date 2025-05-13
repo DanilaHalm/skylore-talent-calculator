49b7ada8ef1732458e4ba50e9b8fe0ad6f3803d0
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-13 10:46:37 +0300

    Update abilities.js

--- src/app/ lib/abilities.js
+++ src/app/ lib/abilities.js
@@ -1,14 +1,20 @@
-import mageSkills from "./mageSkills";
+import { mageUltimates, mageSkills } from "./mageSkills";
 import hunterSkills from "./hunterSkills";
 import shadowSkills from "./shadowSkills";
 import { warriorUltimates, warriorSkills } from "./warriorSkills";
 import alchemistSkills from "./alchemistSkills";
 
 const abilities = {
-  mage: mageSkills,
+  mage: {
+    ultimates: mageUltimates, 
+    skills: mageSkills
+  },
   hunter: hunterSkills,
   shadow: shadowSkills,
-  warrior: { ultimates: warriorUltimates, skills: warriorSkills },
+  warrior: { 
+    ultimates: warriorUltimates, 
+    skills: warriorSkills 
+  },
   alchemist: alchemistSkills,
 };
 
