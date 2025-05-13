e971af007a5ce04e7fb4a7a05d4e0c83a4221049
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-13 12:49:01 +0300

    Update chosedShardsDesc.jsx

--- src/app/components/chosedShardsDesc.jsx
+++ src/app/components/chosedShardsDesc.jsx
@@ -1,6 +1,34 @@
-const ChosedShardsDesc = (skills) => {
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
+const ChosedShardsDesc = (skill) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  const urlSkills = searchParams
+    .get("skills")
+    .split("-")
+  
+  const selectedSkill = urlSkills.find(skill => skill[0] === `${skill.index}`)
+  const shards = [
+  skill.shards[tier1][selectedSkill[1],
+  skill.shards[tier2][selectedSkill[2],
+  skill.shards[tier3][selectedSkill[3]
+  ]
+
+
   return (
     <div className="h-full w-1/2  bg-red-600">
+      {
+        shards.map(shard => {
+            return (
+              <div>
+                <h5>{shard.name}</h5>
+                <p>{shardDesc}</p>
+              </div>
+              )
+          })
+      }
       
     </div>
   )
