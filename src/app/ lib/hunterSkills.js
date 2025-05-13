3a1e47133286868900982e27e578663b699e741e
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-13 08:01:36 +0300

    Update hunterSkills.js

--- src/app/ lib/hunterSkills.js
+++ src/app/ lib/hunterSkills.js
@@ -1,3 +1,22 @@
+const hunterUltimates = [
+  {
+    index: 0,
+    name: "name0",
+    description: "description",
+    requiredLvl: "reqLvl",
+    icon: "icon.jpg",
+    shards: [],
+  },
+  {
+    index: 9,
+    name: "name9",
+    description: "description",
+    requiredLvl: "reqLvl",
+    icon: "icon.jpg",
+    shards: [],
+  },
+];
+
 const hunterSkills = [
   {
     index: 1,
@@ -65,4 +84,4 @@ const hunterSkills = [
   },
 ];
 
-export default hunterSkills;
+export {hunterUltimates,hunterSkills};
