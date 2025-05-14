dcc54eaf5baaeec188c920bda3a6083ca9c5ed20
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-14 18:56:01 +0300

    Update chosedPassives.jsx

--- src/app/components/chosedPassives.jsx
+++ src/app/components/chosedPassives.jsx
@@ -3,23 +3,13 @@
 import { useState } from "react";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
-const ChosedPassives = () => {
+const ChosedPassives = ({passives}) => {
+
   const router = useRouter();
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const [isDescActive, setIsDescActive] = useState(false);
   const urlSkills = searchParams.get("passives").split("");
-
-  const passives = [
-    { name: "Pname0", index: 0, desc: "description0" },
-    { name: "Pname1", index: 1, desc: "description1" },
-    { name: "Pname2", index: 2, desc: "description2" },
-    { name: "Pname3", index: 3, desc: "description3" },
-    { name: "Pname4", index: 4, desc: "description4" },
-    { name: "Pname5", index: 5, desc: "description5" },
-    { name: "Pname6", index: 6, desc: "description6" },
-    { name: "Pname7", index: 7, desc: "description7" },
-  ];
 
   const chosed = passives.filter((skill) => urlSkills.includes(`${skill.index}`));
 
@@ -39,7 +29,7 @@ const ChosedPassives = () => {
       {isDescActive && (
         <div className="flex flex-col">
           {chosed.map((passive) => {
-            return <div key={passive.desc}>{passive.desc}</div>;
+            return <div key={passive.name+passive.desc}>{passive.desc}</div>;
           })}
         </div>
       )}
