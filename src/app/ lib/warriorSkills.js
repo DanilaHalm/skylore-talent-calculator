8c86b0eeb5aa860369203aa8d5a162bd4687c42d
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-14 18:43:22 +0300

    Update warriorSkills.js

--- src/app/ lib/warriorSkills.js
+++ src/app/ lib/warriorSkills.js
@@ -76,8 +76,51 @@ const warriorSkills = [
   },
 ];
 
+const warriorPassives = [
+  { name: "Pname0",
+    index: 0,
+    desc: "description",
+    icon: "icon.jpg"
+  },
+  { name: "Pname1",
+    index: 1,
+    desc: "description",
+    icon: "icon.jpg"
+  },
+  { name: "Pname2",
+    index: 2,
+    desc: "description",
+    icon: "icon.jpg"
+  },
+  { name: "Pname3",
+    index: 3,
+    desc: "description",
+    icon: "icon.jpg"
+  },
+  { name: "Pname4",
+    index: 4,
+    desc: "description",
+    icon: "icon.jpg"
+  },
+  { name: "Pname5",
+    index: 5,
+    desc: "description",
+    icon: "icon.jpg"
+  },
+  { name: "Pname6",
+    index: 6,
+    desc: "description",
+    icon: "icon.jpg"
+  },
+  { name: "Pname7",
+    index: 7,
+    desc: "description",
+    icon: "icon.jpg"
+  },
+]
+
 warriorUltimates.map(ult => ult.shards = warriorShards[ult.index])
 
 warriorSkills.map(skill => skill.shards = warriorShards[skill.index])
 
-export { warriorUltimates, warriorSkills };
+export { warriorUltimates, warriorSkills, warriorPassives };
