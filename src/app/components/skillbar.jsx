4a27a8d0f75bca4ff61d9e40a1ea889089dfb93a
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-14 18:47:10 +0300

    Update skillbar.jsx

--- src/app/components/skillbar.jsx
+++ src/app/components/skillbar.jsx
@@ -10,35 +10,15 @@ import { usePathname } from "next/navigation";
 const SkillBar = () => {
   const pathname = usePathname();
   const charClass = pathname.slice(1);
-  // const ultimates = [
-  //   {
-  //     index: 0,
-  //     name: "name0",
-  //     description: "description",
-  //     requiredLvl: "reqLvl",
-  //     icon: "icon.jpg",
-  //     shards: [],
-  //   },
-  //   {
-  //     index: 9,
-  //     name: "name9",
-  //     description: "description",
-  //     requiredLvl: "reqLvl",
-  //     icon: "icon.jpg",
-  //     shards: [],
-  //   },
-  // ];
-
-  // const [skills, setSkills] = useState(libSkills[charClass]);
-  // const [skills, _] = useState(abilities[charClass].skills);
 
   const skills = abilities[charClass].skills;
   const ultimates = abilities[charClass].ultimates;
+  const passives = abilities[charClass].passives;
 
   return (
     <div className="flex flex-col justify-center content-center items-center bg-red-200">
       <AllSkills skills={skills} ultimates={ultimates} />
-      <AllPassives />
+      <AllPassives passives={passives}/>
       <ChosedSkills skills={[...skills, ...ultimates]} />
       <ChosedPassives />
     </div>
