fe874ad507336d23e21ac74899d41e40541ea0a6
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-20 10:20:26 +0300

    Update classlink.jsx

--- src/app/components/classlink.jsx
+++ src/app/components/classlink.jsx
@@ -8,7 +8,7 @@ const ClassLink = ({ class_link }) => {
   const isSctive = pathname === "/" + class_link.toLowerCase()
   return (
     <div
-      className={`w-14 h-14 ${class_link}-bg ${isActive ? "" : "grayscale"} border-2 bg-cover bg-no-repeat bg-center rounded-full`}
+      className={`w-14 h-14 ${class_link}-bg ${isActive ? "border-green-500" : "grayscale"} border-2 bg-cover bg-no-repeat bg-center rounded-full`}
     >
       <Link className="w-14 h-14 block rounded-full" href={`/${class_link.toLowerCase()}?skills=0000&passives=`}></Link>
     </div>
