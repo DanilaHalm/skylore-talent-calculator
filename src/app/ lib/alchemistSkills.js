0fddd755705591b9efa11f1e061b1f2c733aa233
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-21 12:26:35 +0300

    Update alchemistSkills.js

--- src/app/ lib/alchemistSkills.js
+++ src/app/ lib/alchemistSkills.js
@@ -5,6 +5,15 @@ const alchemistUltimates = [
     index: 0,
     name: "Elixir Of Life",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   //{
   //  index: 9,
@@ -18,53 +27,189 @@ const alchemistSkills = [
     index: 1,
     name: "Directional Transformation",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 2,
     name: "Transmutation Circle",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 3,
     name: "Healing Elixir",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 4,
     name: "Protective Barrier",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 5,
     name: "Unstable Mixture",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 6,
     name: "Healing Transmutation",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 7,
     name: "Potion Making",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 8,
     name: "Distortion Seal",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
 ];
 
 const alchemistPassives = [
-  { name: "No Room For Error", index: 0, desc: "description" },
-  { name: "Careful Handling", index: 1, desc: "description" },
-  { name: "Bracing Tonic", index: 2, desc: "description" },
-  { name: "Helping Hand", index: 3, desc: "description" },
-  { name: "Forbidden Component", index: 4, desc: "description" },
-  { name: "Dispersion", index: 5, desc: "description" },
-  { name: "Arsonist", index: 6, desc: "description" },
-  { name: "Alchemists Revenge", index: 7, desc: "description" },
+  { 
+    name: "No Room For Error",
+    index: 0, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    } 
+  },
+  { 
+    name: "Careful Handling", 
+    index: 1, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }  
+  },
+  { 
+    name: "Bracing Tonic", 
+    index: 2, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    } 
+  },
+  { 
+    name: "Helping Hand", 
+    index: 3, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    } 
+  },
+  { 
+    name: "Forbidden Component", 
+    index: 4, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    } 
+  },
+  { 
+    name: "Dispersion", 
+    index: 5, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    } 
+  },
+  { 
+    name: "Arsonist", 
+    index: 6, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }  
+  },
+  { 
+    name: "Alchemists Revenge", 
+    index: 7, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    } 
+  },
 ];
 
 alchemistUltimates.map((ult) => (ult.shards = alchemistShards[ult.index]));
