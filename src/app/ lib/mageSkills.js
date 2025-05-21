ab52f5261c5183e1dda75e24147c90835e780ad5
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-21 13:21:11 +0300

    Update mageSkills.js

--- src/app/ lib/mageSkills.js
+++ src/app/ lib/mageSkills.js
@@ -5,6 +5,15 @@ const mageUltimates = [
     index: 0,
     name: "Destruction",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   //{
   //  index: 9,
@@ -18,53 +27,189 @@ const mageSkills = [
     index: 1,
     name: "Fireball",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 2,
     name: "Ice Sphere",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 3,
     name: "Energy Wave",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 4,
     name: "Space Tear",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 5,
     name: "Doppelganger",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 6,
     name: "Teleportation",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 7,
     name: "Energy Translocation",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
   {
     index: 8,
     name: "Chain Lightning",
     description: "description",
+    ru: {
+      name: "П",
+      props: [
+        "Активация: Мгновенно",
+        "Цели: На себя",
+        "Тип способности: Заклинание"
+      ],
+      description: "desc",
+    }
   },
 ];
 
 const magePassives = [
-  { name: "Illusory Deceit", index: 0, desc: "description" },
-  { name: "Secret Sight", index: 1, desc: "description" },
-  { name: "Mirror Support", index: 2, desc: "description" },
-  { name: "Heat Haze", index: 3, desc: "description" },
-  { name: "Self-Reliance", index: 4, desc: "description" },
-  { name: "Sorcerers Ingenuity", index: 5, desc: "description" },
-  { name: "Frost Shield", index: 6, desc: "description" },
-  { name: "Energy Absorption", index: 7, desc: "description" },
+  { 
+    name: "Illusory Deceit", 
+    index: 0, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }
+  },
+  { 
+    name: "Secret Sight", 
+    index: 1, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }
+  },
+  { 
+    name: "Mirror Support", 
+    index: 2, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }
+  },
+  { 
+    name: "Heat Haze", 
+    index: 3, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }
+  },
+  { 
+    name: "Self-Reliance", 
+    index: 4, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }
+  },
+  { 
+    name: "Sorcerers Ingenuity", 
+    index: 5, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }
+  },
+  { 
+    name: "Frost Shield", 
+    index: 6, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }
+  },
+  { 
+    name: "Energy Absorption", 
+    index: 7, 
+    desc: "description",
+    ru: {
+      name: "П",
+      description: "desc",
+    }
+  },
 ];
 
 mageUltimates.map((ult) => (ult.shards = mageShards[ult.index]));
