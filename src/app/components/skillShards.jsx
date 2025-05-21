efa500919bc12ac2f141163f0b06acbe53a7460b
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-21 20:35:24 +0300

    Update skillShards.jsx

--- src/app/components/skillShards.jsx
+++ src/app/components/skillShards.jsx
@@ -2,8 +2,8 @@ import Shards from "./shards";
 
 const SkillShards = ({skillIndex}) => {
   return (
-    <div className=" grow bg-purple-300">
-      <div className="flex flex-row justify-around bg-red-400 h-full">
+    <div className="grow">
+      <div className="flex flex-row justify-around shardsBackground rounded-md h-full">
         <Shards skillIndex={skillIndex} shardIndex={1}/>
         <Shards skillIndex={skillIndex} shardIndex={2}/>
         <Shards skillIndex={skillIndex} shardIndex={3}/>
