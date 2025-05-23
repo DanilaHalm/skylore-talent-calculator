dbe14b8d82d3808856486184fd24796f8ad31951
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2025-05-23 10:57:51 +0300

    Update chosedSkills.jsx

--- src/app/components/chosedSkills.jsx
+++ src/app/components/chosedSkills.jsx
@@ -36,7 +36,7 @@ const ChosedSkills = ({ skills }) => {
               <ChosedShardsDesc shards={skill.shards} skillIndex={skill.index} />
             </div>
             {isDescActive && (
-              <div className="h-auto max-w-lg text-xs p-1 bg-no-repeat bg-center bg-cover chosedSkillBg">
+              <div className="h-auto max-w-lg text-xs p-1 bg-no-repeat bg-center bg-cover chosedSkillDescBg">
                 {skill.ru.props.map((prop) => {
                   return <p key={prop}>{prop}</p>;
                 })}
